664c21c9f760b9ab049ffceefd706b62f9697f0a
Illenhad <pierrelegrand.pro@gmail.com>
2019-11-24 13:47:44 +0100

    extracting several pages from a pdf

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     path = input('Enter pdf path: ')
     min_page = int(input('Enter first page: '))
     max_page = int(input('Enter last page: '))
-    pdf_split.split(path, min_page, max_page)
+    pdf_split.extract(path, min_page, max_page)
--- pdf_split.py
+++ pdf_split.py
@@ -2,19 +2,23 @@ import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
-def split(path, min_page, max_page):
+def extract(path, min_page, max_page):
+    """
+    Extract pdf
+    :param path: Path of PDF file
+    :param min_page: number of first page
+    :param max_page: number of last page
+    """
     file_name = os.path.splitext(os.path.basename(path))[0]
-
+    new_name = '{}_new.pdf'.format(file_name)
     pdf = PdfFileReader(path)
+
+    pdf_writer = PdfFileWriter()
     for page in range(min_page-1, max_page):
-        pdf_writer = PdfFileWriter()
+
         pdf_writer.addPage(pdf.getPage(page))
 
-        output_filename = '{}_page_{}.pdf'.format(
-            file_name, page + 1
-        )
+    with open(new_name, 'wb') as out:
+        pdf_writer.write(out)
 
-        with open(output_filename, 'wb') as out:
-            pdf_writer.write(out)
-
-        print('Created: {}'.format(output_filename))
+    print('Created: {}'.format(new_name))
